61431956d9193bb35551bf69f2e5e296bf82e1f8
Stenio Wagner <stenio.wagner1@gmail.com>
2025-01-25 12:33:43 -0300

    chore: setup tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <div className="w-100 h-100 bg-red-200"></div>
+  </StrictMode>
+);
